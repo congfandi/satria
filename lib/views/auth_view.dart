1398f75d5a5eefdf55b3c5c36e100d1500e3dfba
yusufhnf <flyme.yusuf@gmail.com>
2021-11-06 22:20:12 +0700

    ui sign in

--- lib/views/auth_view.dart
+++ lib/views/auth_view.dart
@@ -9,7 +9,10 @@
  */
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:satria/controllers/auth_controller.dart';
+import 'package:satria/views/widgets/auth_button.dart';
 
 class AuthView extends StatefulWidget {
   const AuthView({Key? key}) : super(key: key);
@@ -19,11 +22,76 @@ class AuthView extends StatefulWidget {
 }
 
 class _AuthViewState extends State<AuthView> {
-
   final AuthController _controller = AuthController();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 48),
+              child: SvgPicture.asset("assets/satria.svg"),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                children: [
+                  AuthButton(
+                      iconImage: "assets/google.png",
+                      onPress: () {},
+                      label: "Sign In With Google"),
+                  SizedBox(height: 8.0),
+                  AuthButton(
+                      iconImage: "assets/facebook.png",
+                      onPress: () {},
+                      label: "Sign In With Facebook"),
+                  SizedBox(height: 8.0),
+                  AuthButton(
+                      iconImage: "assets/apple.png",
+                      onPress: () {},
+                      label: "Sign In With Apple"),
+                  SizedBox(height: 16.0),
+                  Column(
+                    children: [
+                      Text(
+                        "Dengan sign in anda menyetujui",
+                        style: TextStyle(fontSize: 12.0),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Text(
+                              "Syarat dan Ketentuan",
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.underline),
+                            ),
+                          ),
+                          Text(
+                            " kami",
+                            style: TextStyle(fontSize: 12.0),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      )),
+    );
   }
 }
