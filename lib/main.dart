ae0d4eb2a71f175025687795e9f8a2886ffab9ba
Cong Fandi <congfandi@gmail.com>
2021-11-07 07:02:34 +0700

    WIP config web application

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,27 @@
  */
 
 import 'dart:async';
+import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-
+import 'package:hive/hive.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'app/my_app.dart';
+
+
+hiveConfig()async{
+   String path = "";
+  if(kIsWeb){
+    path = "/";
+  }else{
+    path = Directory.current.path;
+  }
+   Hive.init(path);
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,6 +41,7 @@ Future<void> main() async {
     sound: true,
   );
   await GetStorage.init();
+  hiveConfig();
   runZonedGuarded(() {
     runApp(const MyApp());
   }, FirebaseCrashlytics.instance.recordError);
